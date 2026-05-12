4f12970205b3693c02928a62f1b906cf97082562
Tobias Villarroel <tobivillarroel023@egmail.com>
2026-05-12 17:51:29 -0300

    Se implementó el módulo de reservas, incluyendo creación, validación de fechas y consulta de vehículos disponibles.

---
+++ models/Reserva.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace alquiler_de_autos.models
+{
+    class Reserva
+    {
+        public DateTime fechaInicio {get; set;}
+        public DateTime fechaFin {get; set;}
+        public Cliente cliente {get; set;}
+        public Vehiculo vehiculo {get; set;}
+
+        public Reserva(DateTime fechaInicio, DateTime fechafin, Cliente cliente, Vehiculo vehiculo)
+        {
+            this.fechaInicio = fechaInicio;
+            this.fechaFin = fechafin;
+            this.cliente = cliente;
+            this.vehiculo = vehiculo;    
+        }
+
+        public override string ToString()
+        {
+            return "Cliente: " + cliente +
+                   "\nVehiculo: " + vehiculo.marca + " " + vehiculo.modelo + " (" + vehiculo.patente + ")" +
+                   "\nFecha desde: " + fechaInicio.ToShortDateString() +
+                   "\nFecha hasta: " + fechaFin.ToShortDateString();
+        }
+    }
+}   
